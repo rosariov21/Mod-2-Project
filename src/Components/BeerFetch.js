bfdd4671473eeeeb91c786e4a8ed2109d160deee
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-14 21:42:06 -0400

    data

--- src/Components/BeerFetch.js
+++ src/Components/BeerFetch.js
@@ -2,34 +2,59 @@ import React, { Component } from "react";
 import axios from 'axios';
 import { render } from "@testing-library/react";
 export default class BeerFetch extends Component{
-    state = {
-        breweries:[],
-        isloading:true
-    };
+    constructor(props){
+        super(props);
+        this.state ={
+            beers: '',
+            beersList:'',
+            food_paring:''
+            
+        }
+    }
 
 
     componentDidMount(){
-        axios.get(`https://api.openbrewerydb.org/breweries`)
-        .then(res => {
-          const breweries = res.data;
-        this.setState({ breweries:breweries,
-        isloading:false });
-        })
+        this.getBeerInfo()
     }
     
     
+    async getBeerInfo(){
+        try{
+        
+        
+        const response = await axios.get('https://api.punkapi.com/v2/beers')
+        console.log(response)
     
-    
-    
+    let beerList = response.data.map(beer =>
+        <div key={beer['id']}>
+
+        <ul>
+            <h1>Name: {beer["name"]}</h1>
+                   <br/>
+            <h1>Best food to go with this beer : {beer["food_pairing"]}</h1>
+        </ul>
+
+
+        </div>);
+
+        this.setState({
+            beers:response.data,
+            beersList:beerList
+        })
+} catch(error){
+    console.error(error);
+}
+}
+
     render(){
-       console.log(this.state.breweries)
+       console.log(this.state.beers)
     
         
         return(
-            <ol>
-        { this.state.breweries.map(brew => <li key={brew.id}><br/>{brew.name}</li>)}
+            <div>
+        { this.state.beersList}
         
-        </ol>
+        </div>
        );
     }
-    } 
+}
