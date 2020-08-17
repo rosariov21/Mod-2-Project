da8c8c931f07aac8839a7d6ffbc7e2c172b6088b
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-17 17:27:48 -0400

    done

--- src/Components/FrontPage.js
+++ src/Components/FrontPage.js
@@ -16,8 +16,8 @@ export default class FrontPage extends Component {
               <img src="https://res.cloudinary.com/teepublic/image/private/s--u-_qG_r9--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_6e2229,e_outline:48/co_6e2229,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1514440809/production/designs/2228531_1.jpg"/>
                <p>Are you ready to search for the perfect beer for you ?</p>
                    <h4>
-                     The objective of this page is to help create all my  beer lovers become more enthused about BEER!<br/>
-                     It's  designed to help you easily find beers so you can brag 
+                     The objective of this page is to help my  beer lover family become more enthused about BEER!<br/>
+                     It's  skecthed out to help you easily find beers so you can brag 
                        to your friends on how much of an expert you are <br/> or just for those who are curious about the true art about beer . 
                    </h4>
                    <h1>Now please , Relax and have a cold one .</h1>
