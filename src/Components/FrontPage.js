ab6e4d8e243e159712d10ef566a8d8f3a828584d
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-17 16:56:54 -0400

    add

--- src/Components/FrontPage.js
+++ src/Components/FrontPage.js
@@ -16,9 +16,9 @@ export default class FrontPage extends Component {
               
                <p>Are you ready to locate the perfect beer?</p>
                    <h4>
-                     The objective of this page is to help create all my  beer lovers become  more enthused about BEER! 
-                     its  designed to help you easily find beers so you can either showoff
-                       to your friends or just for those who are curious about the true art about beer . 
+                     The objective of this page is to help create all my  beer lovers become more enthused about BEER!<br/>
+                     It's  designed to help you easily find beers so you can brag 
+                       to your friends on how much of an expert you are <br/> or just for those who are curious about the true art about beer . 
                    </h4>
                    <h1>Now please , Relax and have a cold one .</h1>
         
