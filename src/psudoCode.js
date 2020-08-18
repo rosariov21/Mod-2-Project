f7123a7dc633ac4504a81808bcf1fe11688dfb67
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-17 23:14:41 -0400

    more

---
+++ src/psudoCode.js
@@ -0,0 +1,22 @@
+//Create three pages 
+//create a webpage 
+// needs to have functions and classes
+// display data 
+// use images witht the api data 
+// good information for user 
+// want the user to feel and have fun 
+// needs to create form 
+// renders info on search button
+// all information will be displayed
+// if user wants a specific beer 
+// user has a choice 
+// needs to have the name of attribut 
+// in order to get that specific information
+// once user searches 
+// user will render all that information
+// in one page 
+// will single display info 
+// the point is to make it fun and simple 
+// bring users to input info 
+// create alert box 
+// give that user a popup to be aware thath they signed up
