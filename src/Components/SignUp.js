3e6a2f8e8885aab03a1ef913d20f4c0e23e6738c
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-17 22:24:34 -0400

    more

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class SignUp extends Component {
-    render() {
-        return (
-            <div className="signUpParent">
+// export default class SignUp extends Component {
+//     render() {
+//         return (
+//             <div className="signUpParent">
                 
-                    <div className='sign-Up-Description-Parent'>
-                        <h2 className="sign-Up-Description">Came to the right page, lets get you started for our weekly Beer Info?</h2>
-                        <p className="sign-Up-Description">Sign up for more information about our Beers </p>
-                    </div>
-                    <div className="footer-newsletter-elements">
-                        <input className="input" type="text" placeholder="email"></input>
-                        <button className="buttonForSignUp" type="submit">Submit</button>
-                    </div>
-                    <div>
-                    <input placeholder="Full name" type='text' />
-                    <button className="buttonForSignUp" type="submit">Submit</button>
-                    </div>
-                </div>
+//                     <div className='sign-Up-Description-Parent'>
+//                         <h2 className="sign-Up-Description">Came to the right page, lets get you started for our weekly Beer Info?</h2>
+//                         <p className="sign-Up-Description">Sign up for more information about our Beers </p>
+//                     </div>
+//                     <div className="footer-newsletter-elements">
+//                         <input className="input" type="text" placeholder="email"></input>
+//                         <button className="buttonForSignUp" type="submit">Submit</button>
+//                     </div>
+//                     <div>
+//                     <input placeholder="Full name" type='text' />
+//                     <button className="buttonForSignUp" type="submit">Submit</button>
+//                     </div>
+//                 </div>
                 
            
-        )
-    }
-}
+//         )
+//     }
+// }
+
